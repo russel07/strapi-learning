e90e621e52788e5aed4298718d51b94ad0d94044
Md. Russel Hussain <md.russel.hussain@gmail.com>
2022-07-20 23:00:43 +0600

    first commit

---
+++ src/api/restauant/routes/restauant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restauant router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::restauant.restauant');
---
+++ src/api/restauant/services/restauant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restauant service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::restauant.restauant');
